f8c977536eefa649cf92f645d057794376dcf6d3
Omar Amin <40728676+oamin12@users.noreply.github.com>
2023-12-01 20:45:06 +0200

    Update mesh.hpp

--- source/common/mesh/mesh.hpp
+++ source/common/mesh/mesh.hpp
@@ -49,7 +49,7 @@ namespace our {
             glBufferData(GL_ELEMENT_ARRAY_BUFFER, elements.size() * sizeof(unsigned int), elements.data(), GL_STATIC_DRAW); //copy the data from elements to the buffer
 
             //set the vertex attribute pointers
-            glVertexAttribPointer(ATTRIB_LOC_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
+            glVertexAttribPointer(ATTRIB_LOC_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
             glEnableVertexAttribArray(ATTRIB_LOC_POSITION);
 
             // in the color attribute, we use the normalized version of the Color data type
@@ -67,9 +67,9 @@ namespace our {
 
 
             //unbind the buffers
+            glBindVertexArray(0);
             glBindBuffer(GL_ARRAY_BUFFER, 0);
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-            glBindVertexArray(0);
 
             
         }
