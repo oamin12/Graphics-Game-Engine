80ddcaaa470cffa1a0c2ea3286aa5227729983e3
AmrAhmed412 <75176768+AmrAhmed412@users.noreply.github.com>
2023-12-30 02:53:59 +0200

    Phase 2: Game (no collision, need to continue rendering rest of elements)

--- source/common/systems/movement.hpp
+++ source/common/systems/movement.hpp
@@ -12,26 +12,43 @@ namespace our
 {
 
     // The movement system is responsible for moving every entity which contains a MovementComponent.
-    // This system is added as a simple example for how use the ECS framework to implement logic. 
+    // This system is added as a simple example for how use the ECS framework to implement logic.
     // For more information, see "common/components/movement.hpp"
-    class MovementSystem {
+    class MovementSystem
+    {
     public:
-
-        // This should be called every frame to update all entities containing a MovementComponent. 
-        void update(World* world, float deltaTime) {
+        // This should be called every frame to update all entities containing a MovementComponent.
+        bool checkCollision(World *world, glm::vec3 position, float radius)
+        {
+            for (auto entity : world->getEntities())
+            {
+                MovementComponent *movement = entity->getComponent<MovementComponent>();
+                if (movement)
+                {
+                    if (glm::distance(position, entity->localTransform.position) < radius)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        void update(World *world, float deltaTime)
+        {
             // For each entity in the world
-            for(auto entity : world->getEntities()){
+            for (auto entity : world->getEntities())
+            {
                 // Get the movement component if it exists
-                MovementComponent* movement = entity->getComponent<MovementComponent>();
+                MovementComponent *movement = entity->getComponent<MovementComponent>();
                 // If the movement component exists
-                if(movement){
+                if (movement)
+                {
                     // Change the position and rotation based on the linear & angular velocity and delta time.
                     entity->localTransform.position += deltaTime * movement->linearVelocity;
                     entity->localTransform.rotation += deltaTime * movement->angularVelocity;
                 }
             }
         }
-
     };
 
 }
